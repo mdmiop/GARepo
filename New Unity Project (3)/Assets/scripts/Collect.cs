c12438caa82f3dd0482b9c03f18b2848b2460e8f
mira <mirapuh@mail.ru>
2020-10-24 23:12:37 +0300

    stuff

---
+++ New Unity Project (3)/Assets/scripts/Collect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    //particle effect to be used
+    public GameObject reward;
+
+    //on trigger enter is a class and will use a 2D collider component 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //test to make sure it is working
+        Debug.Log("collected");
+        //instantiate / create a copy of the reward particle
+        Instantiate(reward, transform.position, transform.rotation);
+        //destroy the original gameobject this script is attached to
+        Destroy(gameObject);
+    }
+}
