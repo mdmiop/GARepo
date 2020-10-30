d19670c6d4d204fd8f0ad81d3e107cd001bea8c8
mira <mirapuh@mail.ru>
2020-10-30 20:17:50 +0200

    complete game

---
+++ New Unity Project (3) copy/Assets/scripts/Collect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Collect : MonoBehaviour
+
+
+{
+    
+    public GameObject reward;
+    
+
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        Debug.Log("collected");
+        
+        Instantiate(reward, transform.position, transform.rotation);
+        
+        Destroy(gameObject);
+
+     
+    }
+
+   
+}
